f66e89a83ed5c3c8cb0764ca0cf5ceca4b8b2352
Lugo_Rio <67293497+Lugo1@users.noreply.github.com>
2022-08-11 19:55:48 -0300

    Update and rename aula_01_apps.js to app.js

--- 001-projeto/app.js
+++ 001-projeto/app.js
@@ -1,7 +1,7 @@
-  var msj = "Pra o Mundo 123... ";
+  var msj = "Pra o Mundo 123...";
   var numero  = 17;
   var combinacao = (numero*0)/0;
-  var indefinicao = (Infinity)*1//1/0//0**(-1);
+  var indefinicao = (Infinity)*1 //Outro exemplo: 0**(-1);
   console.log(msj)
   console.log(typeof msj)
   console.log(numero)
