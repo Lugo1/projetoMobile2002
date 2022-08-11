009ed8edbbceed57bc1596d6da5989f3bb46a737
Lugo_Rio <67293497+Lugo1@users.noreply.github.com>
2022-08-10 21:16:56 -0300

    Add files via upload

---
+++ 001-projeto/aula_01_apps.js
@@ -0,0 +1,12 @@
+  var msj = "Pra o Mundo 123... ";
+  var numero  = 17;
+  var combinacao = (numero*0)/0;
+  var indefinicao = (Infinity)*1//1/0//0**(-1);
+  console.log(msj)
+  console.log(typeof msj)
+  console.log(numero)
+  console.log(typeof numero)
+  console.log(combinacao)
+  console.log(typeof combinacao)
+  console.log(indefinicao)
+  console.log(typeof indefinicao)
