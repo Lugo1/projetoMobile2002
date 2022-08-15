b964c57c74711f5f568e2abe18a18ef46a97d5c8
Lugo_Rio <67293497+Lugo1@users.noreply.github.com>
2022-08-15 20:32:26 -0300

    Add files via upload
    exercicio-005

---
+++ 005-projeto/app.js
@@ -0,0 +1,6 @@
+var str01 = "1"+2+2; //el 1ero considera string y lo resto suma.
+var str02 = 1 + 2 + 2; //considera todos numeros, y suma todo.
+var str03 = 1 + 2 + "2"; // los 2 1eros considera numero y el 3ero string
+console.log(str01);
+console.log(str02);
+console.log(str03);
